73de41739dc2b18b98e49f2c3644ffaa7ed5eb0c
Krish Pathak <krishpathak79@gmail.com>
2026-03-24 22:04:52 +0530

    adding source files

---
+++ models/post.js
@@ -0,0 +1,24 @@
+const { name } = require('ejs');
+const mongoose = require('mongoose');
+
+// mongoose.connect("mongodb://127.0.0:27017/miniproject");
+
+const postSchema =mongoose.Schema({
+    user : {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "User"
+    },
+    date : {
+        type: Date,
+        default: Date.now()
+    },
+    content : String,
+    likes : [{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "User"
+    }]
+
+    
+})
+
+module.exports = mongoose.model("Post", postSchema);
